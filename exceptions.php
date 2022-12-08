ece6fbc5f9eb735e373aee24188102ec3154581a
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-08 12:25:49 +0200

    Add exception example

---
+++ exceptions.php
@@ -0,0 +1,19 @@
+<?php
+
+function inverse($x) {
+  if (!$x) {
+    throw new Exception('Division by zero');
+  }
+  return 1 / $x;
+}
+
+try {
+
+  echo inverse(5);
+  echo inverse(0);
+
+} catch(Exception $e) {
+  echo "Caught Exception", $e -> getMessage();
+} finally {
+  echo "\nFirst finally";
+}
