fa63a3b20382d482aa7ffe2f9ab016dfb2bb90e5
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-17 19:16:01 +0200

    Add methods examples

---
+++ oop/methods.php
@@ -0,0 +1,37 @@
+<?php
+
+class Student {
+
+ var $first_name;
+ var $last_name;
+ var $country = 'None';
+
+ function say_hello() {
+    return "Hello World!";
+ }
+
+}
+
+$student1 = new Student;
+$student1->first_name = 'Maisy';
+$student1->last_name = 'Dog';
+
+
+$student2 = new Student;
+$student2->first_name = 'Jane';
+$student2->last_name = 'Doe';
+
+echo $student1->first_name . " " . $student1->last_name . "<br/>";
+echo $student2->first_name . " " . $student2->last_name . "<br/>";
+echo $student1->say_hello() . "<br/>";
+
+$class_methods = get_class_methods('Student');
+echo "Class methods:" . implode(', ', $class_methods) . "<br/>";
+
+if(method_exists('Student', 'say_hello')) {
+  echo "Method say_hello() exists in Student class. <br/>";
+} else {
+  echo "Method say_hello() does not exists in Student class. <br/>";
+}
+
+?>
